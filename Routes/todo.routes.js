6453e07caf922749d10516dfcad2ee2c48a5b31e
Nanlung <nanlung.temlong@gmail.com>
2021-02-11 12:18:49 +0100

    todo routes

---
+++ Routes/todo.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const todo = require('../Controllers/to-do.controller');
+
+
+
+ // Create a new Todo item
+ router.post("/", todo.create);
+
+ // Retrieve all Todo items
+ router.get("/", todo.findAll);
+
+ 
+ // Retrieve a single Todo with id
+ router.get("/:id", todo.findOne);
+
+ // Update a Todo with id
+ router.put("/:id", todo.update);
+
+ // Delete a Todo item with id
+ router.delete("/:id", todo.delete);
+
+ // Delete all Todo items
+ router.delete("/", tutorials.deleteAll);
+
+ module.exports = router;
