1305029f71faed48f3ae5feba1c819cfaf4935cd
Nanlung <nanlung.temlong@gmail.com>
2021-03-03 12:38:57 +0100

    ListTodos component

---
+++ client/src/components/ListTodo.js
@@ -0,0 +1,35 @@
+import React, {Fragment, useEffect, useState} from 'react';
+
+const ListTodos = () =>  {
+
+  const {todos, setTodos} = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData =  await response.json();
+      setTodos(jsonData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+ 
+  return (<Fragment><table class="table table-dark table-hover mt-5 text-center">
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Description</th>
+      <th>Edit</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  </table></Fragment>
+  )
+};
+
+export default ListTodos;
